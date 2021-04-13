0503a7c837cbdcdf3381495457453e785747d907
Ioann <rus.vanyok@gmail.com>
2021-04-13 15:10:38 +0300

    deploying is amazing!

--- db.php
+++ db.php
@@ -1,8 +1,8 @@
 <?php
 require "libs/rb-mysql.php";
-R::setup( 'mysql:host=ip-10-0-86-170;dbname=heroku_ab4b3ff47a92985',
+R::setup( 'mysql:localhost;dbname=heroku_ab4b3ff47a92985',
     'mysql', 'mysql' );
-if(!R::testConnection()) die('No DB connection!');
+if(!R::testConnection()) die('No DB connection!v1');
 session_start();
 
 /* Heroku remote server */
