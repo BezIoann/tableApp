095772731d0fe6a4a9af9f90173e4aea4e9fb5a9
Ioann <rus.vanyok@gmail.com>
2021-04-13 23:37:54 +0300

    deploying is glorious!

--- db.php
+++ db.php
@@ -9,11 +9,10 @@ $cleardb_db = substr($cleardb_url["path"], 1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
-//$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 //R::setup( 'mysql:host=localhost;dbname=mydatabase',
 //    'user', 'password' );
-R::setup("mysql:host=$cleardb_server;
-        dbname=$cleardb_db","$cleardb_username","$cleardb_password");
+R::setup($conn);
 
 $post = R::dispense('post');
 $post->title = "Hello, world";
