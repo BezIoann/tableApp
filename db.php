d6630de24011ad6ce088cb8ef33257c7fed9398d
Ioann <rus.vanyok@gmail.com>
2021-04-13 12:51:34 +0300

    work with heroku Db

--- db.php
+++ db.php
@@ -1,7 +1,10 @@
 <?php
 require "libs/rb-mysql.php";
-R::setup( 'mysql:host=localhost;dbname=ex4',
+R::setup( 'mysql:host=eu-cdbr-west-01.cleardb.com:3306;dbname=heroku_ab4b3ff47a92985',
     'mysql', 'mysql' );
 if(!R::testConnection()) die('No DB connection!');
 session_start();
+
+/* Heroku remote server */
+
 ?>
