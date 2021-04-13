8db5e1de2387f633a6a2faf4ecf7368d050eec31
Ioann <rus.vanyok@gmail.com>
2021-04-13 22:36:58 +0300

    deploying is glorious!

--- db.php
+++ db.php
@@ -8,6 +8,7 @@ $cleardb_db = substr($cleardb_url["path"], 1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
+require 'rb-mysql.php';
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, "heroku_ab4b3ff47a92985");
 require "libs/rb-mysql.php";
 R::setup("mysql:host=$cleardb_server;
