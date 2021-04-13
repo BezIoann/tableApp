a75af8973823278ca298b17c86a635f47b6e0e64
Ioann <rus.vanyok@gmail.com>
2021-04-13 15:31:02 +0300

    deploying is glorious!

--- db.php
+++ db.php
@@ -1,8 +1,20 @@
 <?php
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"], 1);
+$active_group = 'default';
+$query_builder = TRUE;
+// Connect to DB
+$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 require "libs/rb-mysql.php";
-R::setup( 'mysql:localhost;dbname=heroku_ab4b3ff47a92985',
-    'mysql', 'mysql' );
-if(!R::testConnection()) die('No DB connection!v1');
+R::setup("mysql:host=$cleardb_server;
+        dbname=$cleardb_db","$cleardb_username","$cleardb_password");
+
+if(!R::testConnection()) die('No DB connection!v2');
 session_start();
 
 /* Heroku remote server */
