eb8fa3e560fdf8acc25cad81efbe8c16b9ee56b7
Ioann <rus.vanyok@gmail.com>
2021-04-15 17:29:03 +0300

    ...

--- db.php
+++ db.php
@@ -12,15 +12,8 @@
 //ini_set('memcached.sess_sasl_password', getenv('MEMCACHIER_PASSWORD'));
 require_once "vendor/autoload.php";
 use \RedBeanPHP\R;
-$cleardb_url = parse_url(getenv("mysql://b11148a0327740:d98dda68@eu-cdbr-west-01.cleardb.com/heroku_ab4b3ff47a92985?reconnect=true"));
-$cleardb_server = $cleardb_url["host"];
-$cleardb_username = $cleardb_url["user"];
-$cleardb_password = $cleardb_url["pass"];
-$cleardb_db = substr($cleardb_url["path"], 1);
-$active_group = 'default';
-$query_builder = TRUE;
 R::setup( "mysql:host=eu-cdbr-west-01.cleardb.com; dbname=heroku_ab4b3ff47a92985",
     "b11148a0327740", "d98dda68@" );
-if(!R::testConnection()) die('No DB connection!');
+//if(!R::testConnection()) die('No DB connection!');
 session_start();
 ?>
