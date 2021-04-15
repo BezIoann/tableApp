060d61a5d40302f880c50685422c7eb7118f3c0d
Ioann <rus.vanyok@gmail.com>
2021-04-15 17:45:36 +0300

    ...

--- db.php
+++ db.php
@@ -1,15 +1,5 @@
 <?php
-ini_set('session.save_handler', 'memcached');
-ini_set('session.save_path', getenv('MEMCACHIER_SERVERS'));
-if(version_compare(phpversion('memcached'), '3', '>=')) {
-    ini_set('memcached.sess_persistent', 1);
-    ini_set('memcached.sess_binary_protocol', 1);
-} else {
-    ini_set('session.save_path', 'PERSISTENT=myapp_session ' . ini_get('session.save_path'));
-    ini_set('memcached.sess_binary', 1);
-}
-ini_set('memcached.sess_sasl_username', getenv('MEMCACHIER_USERNAME'));
-ini_set('memcached.sess_sasl_password', getenv('MEMCACHIER_PASSWORD'));
+session_start();
 $cleardb_url = parse_url(getenv("mysql://b11148a0327740:d98dda68@eu-cdbr-west-01"));
 $cleardb_server = $cleardb_url["host"];
 $cleardb_username = $cleardb_url["user"];
@@ -24,5 +14,5 @@ $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $c
 //R::setup( "mysql:host=eu-cdbr-west-01.cleardb.com; dbname=heroku_ab4b3ff47a92985",
 //    "b11148a0327740", "d98dda68@" );
 ////if(!R::testConnection()) die('No DB connection!');
-session_start();
+
 ?>
