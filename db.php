465cb61f8d9aea36169474275126fa4edb30fd2f
Ioann <rus.vanyok@gmail.com>
2021-04-15 17:48:17 +0300

    ...

--- db.php
+++ db.php
@@ -8,7 +8,7 @@ $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
-$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+//$conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
 //require_once "vendor/autoload.php";
 //use \RedBeanPHP\R;
 //R::setup( "mysql:host=eu-cdbr-west-01.cleardb.com; dbname=heroku_ab4b3ff47a92985",
