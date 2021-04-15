60ae37f97d759053b820cb186a5214ec853005f9
Ioann <rus.vanyok@gmail.com>
2021-04-15 19:29:52 +0300

    ...

--- db.php
+++ db.php
@@ -28,5 +28,6 @@ $cleardb_db = substr($cleardb_url["path"],1);
 $active_group = 'default';
 $query_builder = TRUE;
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-
+R::setup("mysql:host=$cleardb_server;
+        dbname=$cleardb_db","$cleardb_username","$cleardb_password");
 ?>
