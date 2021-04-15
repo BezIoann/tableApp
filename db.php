d40d1a01ac9d2710d815699e49837dc2ce4e66ed
Ioann <rus.vanyok@gmail.com>
2021-04-15 19:43:15 +0300

    ...

--- db.php
+++ db.php
@@ -1,6 +1,7 @@
 <?php
 require_once "vendor/autoload.php";
-use \RedBeanPHP\R;
+//use \RedBeanPHP\R;
+//require_once "libs/rb-mysql.php"
 ini_set('session.save_handler', 'memcached');
 ini_set('session.save_path', getenv('MEMCACHIER_SERVERS'));
 if(version_compare(phpversion('memcached'), '3', '>=')) {
@@ -14,11 +15,6 @@ ini_set('memcached.sess_sasl_username', getenv('MEMCACHIER_USERNAME'));
 ini_set('memcached.sess_sasl_password', getenv('MEMCACHIER_PASSWORD'));
 
 session_start();
-if (!isset($_SESSION['count'])) {
-    $_SESSION['count'] = 0;
-}
-$_SESSION['count']++;
-
 echo "Hello #" . $_SESSION['count'];
 $cleardb_url = parse_url(getenv("mysql://b11148a0327740:d98dda68@eu-cdbr-west-01.cleardb.com/heroku_ab4b3ff47a92985?reconnect=true"));
 $cleardb_server = $cleardb_url["host"];
