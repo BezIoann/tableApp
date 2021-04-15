028baaf2bcd28f204f2ef0f503eaad51636f61c6
Ioann <rus.vanyok@gmail.com>
2021-04-16 01:44:19 +0300

    remove mysqli connect

--- db.php
+++ db.php
@@ -22,10 +22,7 @@ $active_group = 'default';
 $query_builder = TRUE;
 // Connect to DB
 $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
-//R::setup( "mysql:host=$cleardb_server;dbname=$cleardb_db",
-//    "$cleardb_username", "$cleardb_password" );
-//$isConnected = R::testConnection();
-//if ($isConnected)
+
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
