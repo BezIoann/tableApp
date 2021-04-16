e1c41651855d7d96f5d60e2d55416fae7d5a7a2b
Ioann <rus.vanyok@gmail.com>
2021-04-16 16:05:42 +0300

    remove mysqli connect

--- logout.php
+++ logout.php
@@ -1,8 +1,8 @@
 <?php
 require "db.php";
 require __DIR__ . '/header.php';
-$_SESSION['logged_user']->status = "offline";
-R::store($_SESSION['logged_user']);
+//$_SESSION['logged_user']->status = "offline";
+//R::store($_SESSION['logged_user']);
 unset($_SESSION['logged_user']);
 exit("<meta http-equiv='refresh' content='0; url= /index.php'>");
 require __DIR__ . '/footer.php';
